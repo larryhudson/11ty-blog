8f1e6f4a02b77e66875b4b1c173ac5af97be49ab
Larry Hudson <larryhudson@hey.com>
2022-06-13 08:19:41 +1000

    add favicon

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,14 @@ module.exports = function (eleventyConfig) {
 
   eleventyConfig.addPassthroughCopy("src/images");
 
+  eleventyConfig.addPassthroughCopy({
+    "src/images/favicon/Larry_48.ico": "favicon.ico",
+    "src/images/favicon/Larry_300.png": "icon.png",
+    "src/images/favicon/Larry_96.png": "favicon-96x96.png",
+    "src/images/favicon/Larry_32.png": "favicon-32x32.png",
+    "src/images/favicon/Larry_16.png": "favicon-16x16.png",
+  });
+
   eleventyConfig.addFilter("monthYear", function (date) {
     console.log(date);
 
