7f9dc197c23d98a8e395b50b904ef473f60a4031
Larry Hudson <larryhudson@hey.com>
2022-11-22 19:41:29 +1100

    remove search

--- netlify/edge-functions/eleventy-edge.js
+++ netlify/edge-functions/eleventy-edge.js
@@ -1,6 +1,5 @@
-import { EleventyEdge } from "https://cdn.11ty.dev/edge@1.0.1/eleventy-edge.js";
+import { EleventyEdge } from "eleventy:edge";
 import precompiledAppData from "./_generated/eleventy-edge-app-data.js";
-import filterProjectsByQuery from "./utils/search-filter.js";
 
 export default async (request, context) => {
   try {
@@ -15,21 +14,7 @@ export default async (request, context) => {
 
     edge.config((eleventyConfig) => {
       // Add some custom Edge-specific configuration
-      eleventyConfig.addGlobalData("searchResults", async () => {
-        const searchQuery = new URL(request.url).searchParams.get("search");
-        if (!searchQuery) return [];
-
-        const requestOrigin = new URL(request.url).origin;
-        const searchParams = new URLSearchParams({
-          query: searchQuery,
-        });
-        const searchUrl =
-          requestOrigin + "/search-data.json?" + searchParams.toString();
-
-        const searchResults = await fetch(searchUrl).then((r) => r.json());
-
-        return searchResults;
-      });
+      // e.g. Fancier json output
       // eleventyConfig.addFilter("json", obj => JSON.stringify(obj, null, 2));
     });
 
